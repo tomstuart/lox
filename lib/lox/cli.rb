35ec994926f222d0168bd954f1135cea966393d3
Tom Stuart <hi@tomstu.art>
2022-02-03 14:52:29 +0000

    Add support for running a Lox file from the CLI
    The acceptance tests do this, so we have to implement it.

--- lib/lox/cli.rb
+++ lib/lox/cli.rb
@@ -3,15 +3,29 @@ module Lox
     def initialize(input, output)
       self.input = input
       self.output = output
+      self.logger = Logger.new(output)
+      self.interpreter = Interpreter.new(output, logger)
     end
 
     def start(args)
-      output.puts "Usage: #{$PROGRAM_NAME} [script]"
-      exit(64)
+      case args
+      in [filename]
+        run_file(filename)
+      else
+        output.puts "Usage: #{$PROGRAM_NAME} [script]"
+        exit(64)
+      end
     end
 
     private
 
-    attr_accessor :input, :output
+    attr_accessor :input, :output, :logger, :interpreter
+
+    def run_file(filename)
+      File.open(filename) do |source|
+        interpreter.run(source)
+      end
+      exit(65) if logger.has_errored?
+    end
   end
 end
--- spec/unit/cli_spec.rb
+++ spec/unit/cli_spec.rb
@@ -4,10 +4,57 @@ require 'lox/cli'
 RSpec.describe Lox::CLI do
   let(:input) { double }
   let(:output) { double }
+  let(:logger) { instance_double('Lox::Logger') }
+  let(:interpreter) { instance_double('Lox::Interpreter') }
 
   subject(:cli) { Lox::CLI.new(input, output) }
 
+  before do
+    class_double('Lox::Logger', new: logger).as_stubbed_const
+    class_double('Lox::Interpreter', new: interpreter).as_stubbed_const
+  end
+
   describe '#start' do
+    context 'with a filename argument' do
+      let(:file) { double }
+      let(:filename) { double }
+      let(:arguments) { [filename] }
+
+      before do
+        allow(File).to receive(:open).and_yield(file)
+        allow(interpreter).to receive(:run)
+        allow(logger).to receive(:has_errored?).and_return(false)
+      end
+
+      it 'opens the file' do
+        cli.start(arguments)
+        expect(File).to have_received(:open).with(filename)
+      end
+
+      it 'evaluates the file' do
+        cli.start(arguments)
+        expect(interpreter).to have_received(:run).with(file)
+      end
+
+      context 'when there are no errors' do
+        it 'returns normally' do
+          expect { cli.start(arguments) }.not_to raise_error
+        end
+      end
+
+      context 'when there are errors' do
+        before do
+          allow(logger).to receive(:has_errored?).and_return(true)
+        end
+
+        it 'exits with status 65 (`EX_DATAERR`)' do
+          expect { cli.start(arguments) }.to raise_error(SystemExit) do |error|
+            expect(error.status).to eq 65
+          end
+        end
+      end
+    end
+
     context 'with unrecognised arguments' do
       let(:output) { StringIO.new }
       let(:arguments) { [double, double] }
